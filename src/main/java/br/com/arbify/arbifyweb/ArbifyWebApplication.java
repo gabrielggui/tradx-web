6dc259249076ed1c6bafaccf8db6bb7647192152
Gabriel Guilherme <gabrielguilherme1000@hotmail.com>
2023-03-12 16:07:55 -0300

    Alteração de nome do projeto: TradX -> Arbify

---
+++ src/main/java/br/com/arbify/arbifyweb/ArbifyWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.arbify.arbifyweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArbifyWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArbifyWebApplication.class, args);
+	}
+
+}
